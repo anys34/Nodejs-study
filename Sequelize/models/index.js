cc32b283fe8f87e56509714c86f0c0c7b98b9343
anys34 <anyeseong34@gmail.com>
2023-07-13 11:21:55 +0900

    ADD ::

--- Sequelize/models/index.js
+++ Sequelize/models/index.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comment');
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config')[env];
@@ -7,5 +9,14 @@ const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
 db.sequelize = sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+User.initiate(sequelize);
+Comment.initiate(sequelize);
+
+User.associate(db);
+Comment.associate(db);
 
 module.exports = db;
